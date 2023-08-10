2fca608dd06961caf16d66670adf5a03e6a26e84
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-08-10 19:06:29 +0300

    Add files via upload

--- XML.js
+++ XML.js
@@ -4,30 +4,29 @@ const countries = document.querySelector('.countries');
 
 
 
-const request = new XMLHttpRequest();
-request.open('GET', 'https://restcountries.com/v3.1/name/deutschland');
 
-request.send();
-console.log(request.responseText)
-request.addEventListener('load', function(){
-    //console.log(this.responseText);
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
-
-    const htmlDeu = `
-        <img class="country_img" src="${data.flags.svg}"/>
-        <h3>${data.name.common}</h3>
-        <p>Population: ${(+data.population / 1000000).toFixed(1)} Millions</p>
-        <p>Language: ${data.languages.deu}</p>
-        <p>Currency: ${data.currencies.EUR.name} ${data.currencies.EUR.symbol}</p>
-    `;
-
-    countries.insertAdjacentHTML('beforeend', htmlDeu);
-});
-
-
+const getCountryData = function(countryName){
+    const request = new XMLHttpRequest();
+        request.open('GET', `https://restcountries.com/v3.1/name/${countryName}`);
+        request.send();
+        
+        request.addEventListener('load', function(){
+            const [data] = JSON.parse(this.responseText);
+        
+            const htmlDeu = `
+                <img class="country_img" src="${data.flags.svg}"/>
+                <h3>${data.name.common}</h3>
+                <p>Population: ${(+data.population / 1000000).toFixed(1)} Millions</p>
+                <p>Language: ${data.languages.deu}</p>
+                <p>Currency: ${data.currencies.EUR.name} ${data.currencies.EUR.symbol}</p>
+            `;
+        
+            countries.insertAdjacentHTML('beforeend', htmlDeu);
+        });
+};
 
 
+getCountryData('deutschland');
 
 
 
