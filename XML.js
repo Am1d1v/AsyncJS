e40690a8e78691057f6d84d0720639cc0cdb09a8
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-08-10 18:44:51 +0300

    Add files via upload

--- XML.js
+++ XML.js
@@ -1,6 +1,6 @@
 'use strict'
 
-
+const countries = document.querySelector('.countries');
 
 
 
@@ -11,8 +11,17 @@ request.send();
 console.log(request.responseText)
 request.addEventListener('load', function(){
     //console.log(this.responseText);
-    const data = JSON.parse(this.responseText);
+    const [data] = JSON.parse(this.responseText);
     console.log(data);
+
+    const html = `
+        <img class="country_img" src="${data.flags.svg}"/>
+        <h3>${data.name.common}</h3>
+        <p>Language: ${data.languages.deu}</p>
+        <p>Currency: ${data.currencies.EUR.name}</p>
+    `;
+
+    countries.insertAdjacentHTML('beforeend', html);
 });
 
 
