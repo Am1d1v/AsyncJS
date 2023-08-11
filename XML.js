5b8cbe8f111163d97bf8f23424b460f5098eff72
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-08-11 18:20:59 +0300

    Add files via upload

--- XML.js
+++ XML.js
@@ -9,24 +9,36 @@ const getCountryData = function(countryName){
     const request = new XMLHttpRequest();
         request.open('GET', `https://restcountries.com/v3.1/name/${countryName}`);
         request.send();
-        
+
         request.addEventListener('load', function(){
             const [data] = JSON.parse(this.responseText);
         
-            const htmlDeu = `
+            const currencies = data.currencies;
+            const currensyName = Object.values(currencies)[0].name;
+            const currensySymbol = Object.values(currencies)[0].symbol;
+            const language = Object.values(data.languages)[0];
+
+
+            const html = `
                 <img class="country_img" src="${data.flags.svg}"/>
                 <h3>${data.name.common}</h3>
                 <p>Population: ${(+data.population / 1000000).toFixed(1)} Millions</p>
-                <p>Language: ${data.languages.deu}</p>
-                <p>Currency: ${data.currencies.EUR.name} ${data.currencies.EUR.symbol}</p>
+                <p>Language: ${language}</p>
+                <p>Currency: ${currensyName} ${currensySymbol}</p>
+                <hr>
             `;
         
-            countries.insertAdjacentHTML('beforeend', htmlDeu);
+            countries.insertAdjacentHTML('beforeend', html);
+            console.log(data);
+        
         });
 };
 
 
-getCountryData('deutschland');
+getCountryData('deutschland', );
+getCountryData('italy');
+getCountryData('usa');
+
 
 
 
