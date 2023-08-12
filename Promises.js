86705bb708fead8a29bd3a9adf45167f4578f642
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-08-12 14:24:32 +0300

    Add files via upload

--- Promises.js
+++ Promises.js
@@ -3,12 +3,22 @@
 
 
 
-const request = fetch('https://restcountries.com/v3.1/name/canada');
 
-console.log(request);
+const getCountryData = function(countryName){
+    fetch(`https://restcountries.com/v3.1/name/${countryName}`)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        //console.log(data[0]);
+        console.log(data[0].name.common);
+        
+    })
+};
 
 
 
+getCountryData('canada');
 
 
 
