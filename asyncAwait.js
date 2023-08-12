cf0f691e2bb8322e6218bbb39108554f160e2158
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-08-12 15:01:02 +0300

    Add files via upload

--- asyncAwait.js
+++ asyncAwait.js
@@ -1,13 +1,36 @@
 
 
 
-
+const countries = document.querySelector('.countries');
 
 const getCountryData = async function(countryName){
     const response = await fetch(`https://restcountries.com/v3.1/name/${countryName}`);
     const data =  await response.json();
-    console.log(data[0]);
+    //console.log(data[0]);
+    displayCountry(data[0]);
 }
 
+const displayCountry = function(data){
+    const currencies = data.currencies;
+    const currensyName = Object.values(currencies)[0].name;
+    const currensySymbol = Object.values(currencies)[0].symbol;
+    const languages = data.languages;
+    const language = Object.values(languages)[0];
+
+    const html = `
+    <img class="country_img" src="${data.flags.svg}"/>
+    <h3>${data.name.common}</h3>
+    <p>Population: ${(+data.population / 1000000).toFixed(1)} Millions</p>
+    <p>Language: ${language}</p>
+    <p>Currency: ${currensyName} ${currensySymbol}</p>
+    <hr>
+
+    `;
+
+    countries.insertAdjacentHTML('beforeend', html);
+
+}
 
 getCountryData('usa');
+getCountryData('australia');
+getCountryData('iceland');
